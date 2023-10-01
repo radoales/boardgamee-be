0683d986d635dca7b2bf7e63a703850d314e8329
radoslav.naydenov <radoslav.naydenov@wult.io>
2023-10-01 22:35:44 +0200

    revert

--- src/server.ts
+++ src/server.ts
@@ -9,43 +9,15 @@ import compression from 'compression'
 import authenticateToken from './middlewares/auth.js'
 import userGamesRouter from './routes/userGames.js'
 import visitedGamesRouter from './routes/visitedGames.js'
-import appInsights from 'applicationinsights'
 
 const createServer = () => {
-  appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY)
-  appInsights.start()
-
   const app = express()
-  app.use(cors()).use(morgan('dev')).use(bodyParser.json()).use(compression())
-  app.use((req, res, next) => {
-    const telemetryClient = appInsights.defaultClient
-
-    telemetryClient.trackRequest({
-      duration: 0,
-      name: req.method + ' ' + req.url,
-      resultCode: res.statusCode,
-      success: true,
-      url: req.url
-    })
-
-    next()
-  })
 
   app
-    .use(
-      (
-        err: Error,
-        req: express.Request,
-        res: express.Response,
-        next: express.NextFunction
-      ) => {
-        const telemetryClient = appInsights.defaultClient
-
-        telemetryClient.trackException({ exception: err })
-
-        res.status(500).send('Internal Server Error')
-      }
-    )
+    .use(cors())
+    .use(morgan('dev'))
+    .use(bodyParser.json())
+    .use(compression())
     .use('/api/users', authenticateToken, usersRouter)
     .use('/api/invitations', authenticateToken, invitationsRouter)
     .use('/api/usergames', authenticateToken, userGamesRouter)
